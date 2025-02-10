53f5556a5432c13a09fda1bd68ac65ed4387fdf3
MW279 <MADOC.WATSON@GMAIL.COM>
2025-02-10 16:02:24 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,10 @@
+export const metaData = {
+  baseUrl: "https://astrofolio-astro.vercel.app/",
+  title: "Madoc Watson",
+  name: "Madoc",
+  ogImage: "/opengraph-image.png",
+};
+
 export const socialLinks = {
   linkedin: "https://www.linkedin.com/in/madoc-watson/",
   email: "mailto:madocwat@terpmail.umd.edu",
